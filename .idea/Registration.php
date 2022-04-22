eee2fee6d8b5812d47e957a6541c72dc7f4b426a
emememem13 <lizavoronina03@mail.ru>
2022-04-22 20:12:07 +0300

    исправили авторизацию

--- .idea/Registration.php
+++ .idea/Registration.php
@@ -1,6 +1,7 @@
 <?php
 
 ?>
+
 <!doctype html>
 <html lang="ru">
 <head>
@@ -14,13 +15,16 @@
     <div class="intro">
         <form  style="margin-top: 0px" method="post" class="RegistrationForm">
             <a class="block" href="">
-                <h3 class="mail">E-mail или имя пользователя</h3>
-                <h3 class="log">Пароль</h3>
+                <h3 class="FIO">ФИО</h3>
+                <h3 class="MAIL">E-mail</h3>
+                <h3 class="PASS">Пароль</h3>
+                <h3 class="PASS2">Повторите пароль</h3>
                 <h3 class="no">Нет аккаунта?</h3>
-                    <input  name="email" type="text" class="InputField1" placeholder="E-mail или имя пользователя">
-                    <input  name="pass" type="text" class="InputField2" placeholder="Пароль">
-                    <button type="submit" name="submit_auth" class="InputButton">Войти</button>
-                <button type="submit" name="submit_auth" class="InputButtonno">Зарегистрироваться</button>
+                <input  name="fio" type="text" class="fio" placeholder="ФИО">
+                <input  name="email" type="text" class="email" placeholder="Логин">
+                <input  name="pass" type="text" class="pass" placeholder="Пароль">
+                <input  name="pass2" type="text" class="pass2" placeholder="Повторите пароль">
+                <button type="submit" name="submit_auth" class="Registr">Зарегистрироваться</button>
             </a>
         </form>
         <hr style="position: absolute;
@@ -37,3 +41,4 @@ border: 1px solid #817E7E;">
 
 </body>
 </html>
+
