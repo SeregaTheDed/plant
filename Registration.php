878819c3b81f19108f17da832db7a9a5662902ef
emememem13 <lizavoronina03@mail.ru>
2022-04-22 20:34:07 +0300

    форма регистрации

--- Registration.php
+++ Registration.php
@@ -19,7 +19,6 @@
                 <h3 class="MAIL">E-mail</h3>
                 <h3 class="PASS">Пароль</h3>
                 <h3 class="PASS2">Повторите пароль</h3>
-                <h3 class="no">Нет аккаунта?</h3>
                 <input  name="fio" type="text" class="fio" placeholder="ФИО">
                 <input  name="email" type="text" class="email" placeholder="Логин">
                 <input  name="pass" type="text" class="pass" placeholder="Пароль">
@@ -27,13 +26,7 @@
                 <button type="submit" name="submit_auth" class="Registr">Зарегистрироваться</button>
             </a>
         </form>
-        <hr style="position: absolute;
-width: 430.01px;
-height: 0px;
-left: 470px;
-top: 460px;
 
-border: 1px solid #817E7E;">
     </div>
 
 </div>
